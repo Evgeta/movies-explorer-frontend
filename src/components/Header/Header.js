5e270d6c1f90463a4b58702db30e469e0ba23a4f
Evgenia Tarasyuk <evgeta@yandex.ru>
2022-09-01 20:45:52 +0300

    feat: add header navigation block

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import './Header.css';
+
+function Header() {
+      
+    return (
+      <header className="header">
+        <div className="header__container">
+          
+        </div>
+      </header>
+    );
+  }
+  
+  export default Header;
+  
