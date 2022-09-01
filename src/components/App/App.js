0905a7c107b611cdfd5491d07430fa7ddadabb06
Evgenia Tarasyuk <evgeta@yandex.ru>
2022-09-01 16:25:32 +0300

    feat: add configuration and folder structure

---
+++ src/components/App/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+
+    
+    return (
+      <div className="app">
+          {/* <Header /> */}
+      {/* <Switch> 
+      <ProtectedRoute
+              exact path="/"
+              component={Main}
+            >
+        </ProtectedRoute>
+        <Route path="/sign-in">
+          <Login handleLogin={handleLogin}/>
+        </Route>
+        <Route path="/sign-up">
+          <Register handleRegistration={handleRegistration}/>
+        </Route>
+     </Switch>    */}
+    </div>
+    );}
+  
+  export default App;
+  
---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+//настройка доступа к API
+ //export const baseUrl = 'http://localhost:3000';
+
+ export const baseUrl = 'https://api.mesto.evgeta.nomoredomains.sbs';
+
