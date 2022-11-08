8120e148077fcddf2c6a5b03391a8837f606d2c6
Evgenia Tarasyuk <evgeta@yandex.ru>
2022-11-08 22:15:50 +0300

    fix: styles of MoviesCard and MoviesCardList

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,28 +1,47 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 import "./MoviesCard.css";
+import movieimage from "../../images/movie-image.png";
 
-function MoviesCard() {
+function MoviesCard({ saved = false, title }) {
+  const location = useLocation().pathname;
+
   return (
     <li className="movies-card">
       <article className="movies-card__item">
-        <a target="_blank" href="" className="movies-card__image-link">
+        <div className="movies-card__description">
+          <h2 className="movies-card__title">{title}</h2>
+          <span className="movies-card__duration">1ч 42м</span>
+          {location === "/movies" && saved ? ( //если отображаем все фильмы
+            <button
+              type="button"
+              className="movies-card__button movies-card__button_type_saved"
+              title="Сохранить"
+            ></button>
+          ) : (
+            <button
+              type="button"
+              className="movies-card__button movies-card__button_type_not-saved"
+              title="Сохранить"
+            ></button>
+          )}
+          {location === "/saved-movies" && ( //если отображаем сохраненные фильмы
+            <button
+              type="button"
+              className="movies-card__button movies-card__button_type_delete-icon"
+              title="Сохранить"
+            ></button>
+          )}
+        </div>
+        <a target="_blank" href="" rel="noopener noreferrer" className="movies-card__image-link">
           <img
-            src="./images/movie-image.png"
-            alt="33 слова о дизайне"
-            title="Описание: Война искусств"
+            src={movieimage}
+            alt={title}
+            title={title}
             className="movies-card__image"
           />
         </a>
-        <div className="movies-card__description">
-          <h2 className="movies-card__title">33 слова о дизайне</h2>
-          <button
-            type="button"
-            className="movies-card__button movies-card__button_type_saved"
-            title="Сохранить"
-          ></button>
-        </div>
-        <span className="movies-card__duration">1ч 42м</span>
       </article>
     </li>
   );
--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -3,231 +3,50 @@ import React from "react";
 import "./MoviesCardList.css";
 import "../MoviesCard/MoviesCard.css";
 
-import movieimage from "../../images/movie-image.png";
+// import movieimage from "../../images/movie-image.png";
+import MoviesCard from "../MoviesCard/MoviesCard";
 
 function MoviesCardList() {
   return (
     <div className="movies-cardlist">
-      <ul className="movies-cardlist__list">
-        <li className="movies-card">
-          <article className="movies-card__item">
-            <a
-              target="_blank"
-              href=""
-              rel="noopener noreferrer"
-              className="movies-card__image-link"
-            >
-              <img
-                src={movieimage}
-                alt="33 слова о дизайне"
-                title="Описание: Война искусств"
-                className="movies-card__image"
-              />
-            </a>
-            <div className="movies-card__description">
-              <h2 className="movies-card__title">33 слова о дизайне</h2>
-              <button
-                type="button"
-                className="movies-card__button movies-card__button_type_saved"
-                title="Сохранить"
-              ></button>
-            </div>
-            <span className="movies-card__duration">1ч 42м</span>
-          </article>
-        </li>
-
-        <li className="movies-card">
-          <article className="movies-card__item">
-            <a
-              target="_blank"
-              href=""
-              rel="noopener noreferrer"
-              className="movies-card__image-link"
-            >
-              <img
-                src={movieimage}
-                alt="Война искусств"
-                title="Описание: Война искусств"
-                className="movies-card__image"
-              />
-            </a>
-            <div className="movies-card__description">
-              <h2 className="movies-card__title">
-                Киноальманах «100 лет дизайна»
-              </h2>
-              <button
-                type="button"
-                className="movies-card__button movies-card__button_type_saved"
-                title="Сохранить"
-              ></button>
-            </div>
-            <span className="movies-card__duration">1ч 42м</span>
-          </article>
-        </li>
-
-        <li className="movies-card">
-          <article className="movies-card__item">
-            <a
-              target="_blank"
-              href=""
-              rel="noopener noreferrer"
-              className="movies-card__image-link"
-            >
-              <img
-                src={movieimage}
-                alt="Война искусств"
-                title="Описание: Война искусств"
-                className="movies-card__image"
-              />
-            </a>
-            <div className="movies-card__description">
-              <h2 className="movies-card__title">
-                Gimme Danger: История Игги и The Stooges
-              </h2>
-              <button
-                type="button"
-                className="movies-card__button movies-card__button_type_not-saved"
-                title="Сохранить"
-              ></button>
-            </div>
-            <span className="movies-card__duration">1ч 42м</span>
-          </article>
-        </li>
-
-        <li className="movies-card">
-          <article className="movies-card__item">
-            <a
-              target="_blank"
-              href=""
-              rel="noopener noreferrer"
-              className="movies-card__image-link"
-            >
-              <img
-                src={movieimage}
-                alt="Война искусств"
-                title="Описание: Война искусств"
-                className="movies-card__image"
-              />
-            </a>
-            <div className="movies-card__description">
-              <h2 className="movies-card__title">Война искусств</h2>
-              <button
-                type="button"
-                className="movies-card__button movies-card__button_type-saved"
-                title="Сохранить"
-              ></button>
-            </div>
-            <span className="movies-card__duration">1ч 42м</span>
-          </article>
-        </li>
-
-        <li className="movies-card">
-          <article className="movies-card__item">
-            <a
-              target="_blank"
-              href=""
-              rel="noopener noreferrer"
-              className="movies-card__image-link"
-            >
-              <img
-                src={movieimage}
-                alt="Война искусств"
-                title="Описание: Война искусств"
-                className="movies-card__image"
-              />
-            </a>
-            <div className="movies-card__description">
-              <h2 className="movies-card__title">Война искусств</h2>
-              <button
-                type="button"
-                className="movies-card__button movies-card__button_type-saved"
-                title="Сохранить"
-              ></button>
-            </div>
-            <span className="movies-card__duration">1ч 42м</span>
-          </article>
-        </li>
-
-        <li className="movies-card">
-          <article className="movies-card__item">
-            <a
-              target="_blank"
-              href=""
-              rel="noopener noreferrer"
-              className="movies-card__image-link"
-            >
-              <img
-                src={movieimage}
-                alt="Война искусств"
-                title="Описание: Война искусств"
-                className="movies-card__image"
-              />
-            </a>
-            <div className="movies-card__description">
-              <h2 className="movies-card__title">Война искусств</h2>
-              <button
-                type="button"
-                className="movies-card__button movies-card__button_type-saved"
-                title="Сохранить"
-              ></button>
-            </div>
-            <span className="movies-card__duration">1ч 42м</span>
-          </article>
-        </li>
-
-        <li className="movies-card">
-          <article className="movies-card__item">
-            <a
-              target="_blank"
-              href=""
-              rel="noopener noreferrer"
-              className="movies-card__image-link"
-            >
-              <img
-                src={movieimage}
-                alt="Война искусств"
-                title="Описание: Война искусств"
-                className="movies-card__image"
-              />
-            </a>
-            <div className="movies-card__description">
-              <h2 className="movies-card__title">Война искусств</h2>
-              <button
-                type="button"
-                className="movies-card__button movies-card__button_type-saved"
-                title="Сохранить"
-              ></button>
-            </div>
-            <span className="movies-card__duration">1ч 42м</span>
-          </article>
-        </li>
-
-        <li className="movies-card">
-          <article className="movies-card__item">
-            <a
-              target="_blank"
-              href=""
-              rel="noopener noreferrer"
-              className="movies-card__image-link"
-            >
-              <img
-                src={movieimage}
-                alt="Война искусств"
-                title="Описание: Война искусств"
-                className="movies-card__image"
-              />
-            </a>
-            <div className="movies-card__description">
-              <h2 className="movies-card__title">Война искусств</h2>
-              <button
-                type="button"
-                className="movies-card__button movies-card__button_type_saved"
-                title="Сохранить"
-              ></button>
-            </div>
-            <span className="movies-card__duration">1ч 42м</span>
-          </article>
-        </li>
+      <ul className="movies-cardlist__list">        
+      <MoviesCard
+        saved={true}
+        title={'Описание: Война искусств'}
+      />
+      <MoviesCard
+        saved={false}
+        title={'Киноальманах «100 лет дизайна»'}
+      />
+      <MoviesCard
+        saved={false}
+        title={'Gimme Danger: История Игги и The Stooges'}
+      />
+      <MoviesCard
+        saved={true}
+        title={'Описание: Война искусств'}
+      />
+      <MoviesCard
+        saved={false}
+        title={'Киноальманах «100 лет дизайна»'}
+      />
+      <MoviesCard
+        saved={false}
+        title={'Gimme Danger: История Игги и The Stooges'}
+      />
+      <MoviesCard
+        saved={true}
+        title={'Описание: Война искусств'}
+      />
+      <MoviesCard
+        saved={false}
+        title={'Киноальманах «100 лет дизайна»'}
+      />
+      <MoviesCard
+        saved={false}
+        title={'Gimme Danger: История Игги и The Stooges'}
+      />
+        
       </ul>
       <button className="movies-cardlist__showmore">Ещё</button>
     </div>
