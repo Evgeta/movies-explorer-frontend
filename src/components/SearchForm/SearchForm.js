e69fba7f9a1ea6dc14557836cef779e6badb1b2f
Evgenia Tarasyuk <evgeta@yandex.ru>
2022-11-13 16:20:48 +0300

    fix: adjust layout of movies at 320px

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -15,7 +15,7 @@ function SearchForm() {
           />
           <button className="search-form__button" type="submit"></button>
         </form>
-        <div className="search-form__decorline">
+        <div className="search-form__decorline"> </div>         
         <label className="search-form__filter">
           <input
             className="search-form__checkbox"
@@ -25,7 +25,7 @@ function SearchForm() {
           <span className="search-form__switcher"></span>
           <span className="search-form__text">Короткометражки</span>
         </label>
-        </div>                
+               
       </div>
       <hr className="search-form__horizontal-decor-line"></hr>
     </section>
